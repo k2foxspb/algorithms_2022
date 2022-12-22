ed3fa17e4225f414d8ab63ff4f93fce941c1f21f
k2foxspb <k2foxpsb@mail.ru>
2022-12-22 23:19:45 +0300

    lesson_3

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -12,6 +12,8 @@
 ОБЯЗАТЕЛЬНО! Добавьте аналитику: что вы сделали и какой это принесло эффект
 """
 
+from timeit import timeit
+
 
 def func_1(nums):
     new_arr = []
@@ -19,3 +21,22 @@ def func_1(nums):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+time_1 = timeit('func_1(range(50))', globals=globals())
+print(f'время выполнения функции: {time_1}')
+
+
+def func_2(nums):
+    return [i for i in range(len(nums)) if i % 2 == 0]
+
+
+time_2 = timeit('func_2(range(50))', globals=globals())
+print(f'время выполнения оптимизированной функции: {time_2}')
+
+
+'''
+время выполнения функции: 5.3306213999167085
+время выполнения оптимизированной функции: 2.721817099954933
+
+list comprehension гораздо быстрее заполняет список и выглядит более лаконично!
+'''
